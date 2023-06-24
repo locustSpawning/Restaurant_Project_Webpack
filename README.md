25b67abe2070479f8388d3176022a533a42ae99a
chels <chelsleonhardt@gmail.com>
2023-06-24 17:56:28 -0400

    Bundled webpack - added elements to doc using js -next step adding images and css

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Restaurant_Project_Webpack
 Using webpack for the first time!
+
+
+photos ~
+Image by <a href="https://pixabay.com/users/krislandina-14815800/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4742463">Kris L</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4742463">Pixabay</a>
+
+Blue Orchid Creations Photography 
+
+Image by <a href="https://pixabay.com/users/jillwellington-334088/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2107191">Jill Wellington</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2107191">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/inspirecreatecelebrate-72462/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3395264">Erin</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3395264">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/terric-4014469/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2107599">Terri Cnudde</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2107599">Pixabay</a>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
 import _ from 'lodash';
+import Images from '../images';  //figure out importing images from a folder
 
 console.log('whaaat?');
 
-function component() {
-    const element = document.createElement('div');
-    const Home = document.createElement('button');
-  
-     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    Home.innerHTML = (['Home']);
-  
-    return Home;
-  }
-  
-  document.body.appendChild(component());
+var Home;
+var Menu;
+var Contact;
+var Greeting;
+var Description;
+var openingImage;
+
+
+function addButton(){
+    const button = document.createElement('button');
+    return button;
+}
+
+
+Home = addButton();
+Home.innerHTML = (['Home']);
+document.body.appendChild(Home);
+
+Menu = addButton();
+Menu.innerHTML = (['Menu']);
+document.body.appendChild(Menu);
+
+Contact = addButton();
+Contact.innerHTML = (['Contact']);
+document.body.appendChild(Contact);
+
+
+
+function addHeader1(){
+    const h1 = document.createElement('h1');
+    return h1;
+}
+
+function addHeader3(){
+    const h2 = document.createElement('h3');
+    return h2;
+}
+
+Greeting = addHeader1();
+Greeting.innerHTML = (['Mademoiselle Macaron']);
+document.body.appendChild(Greeting);
+
+Description = addHeader3();
+Description.innerHTML = (['Authentic French bakery ~ made with love']);
+document.body.appendChild(Description);
+
+
+function addImage(){
+    const img = document.createElement('image');
+    return img;
+}
+
+openingImage = addImage();
+openingImage.src = (['../images/macarons.jpg']);
+document.body.appendChild(openingImage);
